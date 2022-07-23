7a74f234bcc3f85ee112a2887e6f795e5262b721
denis <denispernach@yandex.ru>
2022-07-23 16:57:56 +0200

    zadaza 15

---
+++ task15/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число дня недели: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+switch (number1)
+{
+    case 1:
+    Console.WriteLine("нет рабочий день");
+    break;
+
+    case 2:
+    Console.WriteLine("нет рабочий день");
+    break;
+
+    case 3:
+    Console.WriteLine("нет рабочий день");
+    break;
+
+    case 4:
+    Console.WriteLine("нет рабочий день");
+    break;
+
+    case 5:
+    Console.WriteLine("нет рабочий день");
+    break;
+
+    case 6:
+    Console.WriteLine("да выходной");
+    break;
+
+    case 7:
+    Console.WriteLine("да выходной");
+    break;
+
+    default:
+    Console.WriteLine("Нет такого дня недели");
+    break;
+    }
