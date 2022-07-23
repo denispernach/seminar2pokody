2fd2d17c771f2ccabeb4ecba6a01508a2fac59fb
denis <denispernach@yandex.ru>
2022-07-23 15:54:38 +0200

    zadaza 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5  782 -> 8  918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberA = number / 100;
+int numberB = number / 10 % 10;
+int numberC = number % 10;
+int result = numberB;
+Console.WriteLine($"{result}");
+
