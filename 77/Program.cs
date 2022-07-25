08df0e03d057bead74befbc8c8c0a66a5faebc71
denis <denispernach@yandex.ru>
2022-07-25 09:51:36 +0200

    zadaza 15 исправлено

---
+++ 77/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<=5)
+{ 
+    Console.WriteLine("рабочий");
+}
+if (number > 5 && number <= 7) 
+{ 
+    Console.WriteLine("да выходной");
+}
+else 
+{
+    Console.WriteLine("Нет такого дня недели");
+}
